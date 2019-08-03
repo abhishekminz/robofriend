12c449531f27c593fa79d4310622cead53d962a9
Abhishek Minz <51510507+abhishekminz@users.noreply.github.com>
2019-08-03 13:40:55 +0530

    Updates

---
+++ precache-manifest.cd86cb8747ff2c33b0effb59b097cecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e232d31e1a4f8b7f80f5d6519f2103d",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9709264669f153aadafe",
+    "url": "/robofriend/static/css/main.02f92a94.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriend/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "9709264669f153aadafe",
+    "url": "/robofriend/static/js/main.56ed26bf.chunk.js"
+  },
+  {
+    "revision": "75cc8018399ebb82c64f",
+    "url": "/robofriend/static/js/runtime~main.f5f06c82.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
